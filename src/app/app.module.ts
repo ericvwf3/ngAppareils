ae919912f96cca93b00cc731141b8f6c643dc80f
ericvwf3 <ericvwf3@gmail.com>
2018-12-29 07:43:40 +0100

    Add guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { FormsModule } from '@angular/forms';
-import { AppareilService } from './services/appareil.services';
+import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import { Routes, RouterModule } from '@angular/router'
-import { AuthService } from './services/auth.services';
+import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: 'appareils', component: AppareilViewComponent},
-  { path: 'appareils/:id', component: SingleAppareilComponent},
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
+  { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
   { path: 'auth', component: AuthComponent},
   { path: '', component: AppareilViewComponent},
   { path: 'not-found', component: FourOhFourComponent },
-  { path: '**', redirectTo: 'not-found' }
+  { path: '**', redirectTo: '/not-found' }
 
 ];
 
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     AppareilService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
