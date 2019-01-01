a9395bcedc8222a8896824c9e431a59fd8661e1d
ericvwf3 <ericvwf3@gmail.com>
2019-01-01 18:51:16 +0100

    add reactive method

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { User } from '../models/User.model';
+import { Subject } from 'rxjs';
+
+export class UserService {
+    private users: User[] = [
+        {
+            firstName: 'James',
+            lastName: 'Smith',
+            email: 'james@smith.com',
+            drinkPreference: 'Coca',
+            hobbies: [
+                'Coder',
+                'La dégustation de café'
+            ]
+        }
+    ];
+    userSubject = new Subject<User[]>();
+
+    emitUsers() {
+        this.userSubject.next(this.users.slice());
+    }
+
+    addUser(user: User) {
+        this.users.push(user);
+        this.emitUsers();
+    }
+
+}
+
